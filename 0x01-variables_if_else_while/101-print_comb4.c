784af0b2d994ee5c1dcddfbe4841d8d57eea82e4
Nariman <narimanyasser4444@gmail.com>
2023-03-20 13:23:23 -0700

    a

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 
 /**
- * 
- * Description: point all possible diffrent
- 	       combinations of digits
-*
-* Rutern: Always 0 (success)
+* main - prints all possible different combinations of three digits
+* Return: Always 0 (Success)
 */
+
 int main(void)
 {
-	int digit1 = 0;
-	int digit2 , digit3; 
+	int n, m, l;
 
-	while(digit1 <= 9)
+	for (n = 48; n < 58; n++)
 	{
-	      digit2 = 0;
-	while(digit2 <= 9)
-	   {
-		   digit3 = 0;
-		   while(digit3 <=9)
+		for (m = 49; m < 58; m++)
+		{
+			for (l = 50; l < 58; l++)
 			{
-				if(digit
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
