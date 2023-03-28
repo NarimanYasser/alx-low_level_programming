e8c49f010c638fb8a8ac89482d416247da494d39
Nariman <narimanyasser4444@gmail.com>
2023-03-28 15:58:02 -0700

    a

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,12 +7,11 @@
 
 int _atoi(char *s)
 {
-		int sign;
-			unsigned int num;
-				char *temp;
+	int sign;
+	unsigned int num;
+	char *temp;
 
-				temp = s;
-					num = 0;
+	temp = s;					num = 0;
 						sign = 1;
 							while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 									{
