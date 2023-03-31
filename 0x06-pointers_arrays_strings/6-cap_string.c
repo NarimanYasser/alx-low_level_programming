44c13e071733a07047ad44d2e799e6e1ceb616b7
Nariman <narimanyasser4444@gmail.com>
2023-03-30 17:02:41 -0700

    a

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *cap_string - Description
+ *  @c: inputted string
+ *
+ * Return: returns a converted string
+ */
+char *cap_string(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		if (c[i] > 96 && c[i] < 123 && (c[i - 1] == ' ' ||
+						c[i - 1] == '\n' || c[i - 1] == '\t' || c[i - 1] == ','
+						|| c[i - 1] == ';' || c[i - 1] == '.' || c[i - 1] == '!'
+						|| c[i - 1] == '?' || c[i - 1] == '"' || c[i - 1] == '(' ||
+						c[i - 1] == ')' || c[i - 1] == '{' || c[i - 1] == '}' || i == 0))
+			c[i] = c[i] - 32;
+		i++;
+	}
+
+	return (c);
+}
