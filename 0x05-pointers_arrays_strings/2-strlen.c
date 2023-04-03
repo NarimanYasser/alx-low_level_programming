bc3c2ab5e40523b47f2ff2eb74192ce6407a914a
Nariman <narimanyasser4444@gmail.com>
2023-04-03 15:50:09 -0700

    a

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,7 @@
-nclude "main.h"
+#include "main.h"
 
 /**
-<<<<<<< HEAD
- * _strlen -length of a string
-=======
  * _strlen - Function that returns the length of a string
->>>>>>> df279ee97f370a2b34983e55ccce1c66245779b5
  *
  * @s: this is the input string
  *
@@ -15,11 +11,7 @@ int _strlen(char *s)
 {
 	int index;
 
-<<<<<<< HEAD
 	for (index = 0; s[index] != '\0' ; index++)
-=======
-        for (index = 0; s[index] != '\0' ; index++)
->>>>>>> df279ee97f370a2b34983e55ccce1c66245779b5
 		;
 	return (index);
 }
