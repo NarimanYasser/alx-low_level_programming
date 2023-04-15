cab5b1ceeb2ec8491a7e6d47b93c8407578b7f5f
Nariman <narimanyasser4444@gmail.com>
2023-04-15 01:39:56 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *  @argc: integer value counting the number of arguments
+ *  @argv: array of strings
+ *  Return: it returns 0 if the program compiles ok
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
