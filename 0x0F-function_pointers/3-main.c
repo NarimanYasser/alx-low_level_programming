8e498ba8b7260bf4e98ec5acc6266494eadfcdea
Nariman <narimanyasser4444@gmail.com>
2023-04-19 19:48:11 -0700

    a

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,9 +20,15 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
 	op_func = get_op_func(argv[2]);
 
-	if (!op_func)
+	if (op_func == NULL)
 	{
 	       printf("Error\n");
 		exit(99);
