c2e24f04f279c66781e89b4c168845196a0db285
NarimanYasser <narimanyasser4444@gmail.com>
2023-08-16 17:24:45 -0700

    a

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -21,22 +21,21 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	if (argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	op_func = get_op_func(argv[2]);
-
-	if (op_func == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
+
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
 	printf("%d\n", op_func(a, b));
 	return (0);
