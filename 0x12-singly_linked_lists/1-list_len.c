309b4b182fdf7002c03f6141d7211eb2aef705e5
NarimanYasser <narimanyasser4444@gmail.com>
2023-08-24 16:09:10 -0700

    a

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *  print_list - The elements of list_t list.
+ *
+ *  @h: pointer to the head nodes.
+ *
+ *  Return: The number of nodes.
+ *
+ */
+size_t list_len(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h == NULL)
+	{
+		h = h->next;
+		nodes++;
+	}
+
+	return (nodes);
+}
