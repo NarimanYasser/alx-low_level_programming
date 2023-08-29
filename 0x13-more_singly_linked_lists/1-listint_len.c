7e71e868bf2c98290fd50c5339395bcfa8e21e46
NarimanYasser <narimanyasser4444@gmail.com>
2023-08-29 15:23:22 -0700

    a

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - The prints all the elements of a list.
+ * @h: The pointer of a list.
+ *
+ * Return: The size of the list.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
