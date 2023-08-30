5a52d038823d8678161fd57fb9cb1f136c09f8cf
NarimanYasser <narimanyasser4444@gmail.com>
2023-08-29 19:07:45 -0700

    a

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	node = *head;
 	while (node)
 	{
-		if (i  == index - 1)
+		if (i  == index)
 		{
 			prev->next = node->next;
 			free(node);
