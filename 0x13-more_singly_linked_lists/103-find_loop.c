f3234afcea0785f482a36d24ba434b7da1d1f186
NarimanYasser <narimanyasser4444@gmail.com>
2023-08-29 20:02:10 -0700

    a

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * find_listint_loop - the find loop
+ *
+ * @head: the pointer.
+ *
+ * Return: the address.
+ *
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *a, *b;
+
+	if (head == NULL)
+		return (NULL);
+	for (b = head->next; b != b->next)
+	{
+		if (b == b->next)
+			return (b);
+		for (a = head; a != b; a = a->next)
+			if (a == b->next)
+				return (b->next);
+	}
+}
